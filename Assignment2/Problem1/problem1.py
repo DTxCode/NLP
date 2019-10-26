4f8d41ba4787abc79ce9c990a6784fa307011858
David Tandetnik <dave.tand@gmail.com>
2019-10-26 18:00:22 -0400

    assignment 2, problems 1 and 2

--- Assignment2/Problem1/problem1.py
+++ Assignment2/Problem1/problem1.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
 import os
 import spacy
 from io import open
+from collections import Counter
+
 nlp = spacy.load("en_core_web_sm")
 nlp.max_length = 6000000  # Warning: requires large amount of RAM
 
@@ -17,23 +21,55 @@ for trainFilePath in trainFilePaths:
 
     trainFile.close()
 
-
-doc = nlp(trainFilesContents)
+doc = nlp(unicode(trainFilesContents))
 
 numberOfSentences = 0
 for sent in doc.sents:
     numberOfSentences += 1
 
 numberOfVerbs = 0
+numberOfPreps = 0
+preps = []
 for token in doc:
     if token.pos_ == "VERB":
         numberOfVerbs += 1
+    if token.dep_ == "prep":
+        numberOfPreps += 1
+        preps.append(token.text)
 
+prepsFreq = Counter(preps)
+mostCommonPreps = prepsFreq.most_common(3)
+mostCommonPrepNames = [str(tuple[0]) for tuple in mostCommonPreps]
+
+uniqueEntities = set()
+totalEntities = 0
+for ent in doc.ents:
+    totalEntities += 1
+    uniqueEntities.add(str(ent.label_))
 
 print("Number of sentences: " + str(numberOfSentences))
 print("Number of verbs: " + str(numberOfVerbs) +
       ". Average per sentence: " + str(float(numberOfVerbs) / numberOfSentences))
+print("Number of prepositions: " + str(numberOfPreps) + ". Three most common prepositions: " + str(mostCommonPrepNames))
+print("Number of entities: " + str(totalEntities) + ". Unique entity lables: " + str(uniqueEntities))
 
-# for token in doc:
-#     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-#           token.shape_, token.is_alpha, token.is_stop)
+# look at all the dependency parsings
+depMap = {}
+for token in doc:
+    if str(token.dep_) not in depMap:
+        depMap[str(token.dep_)] = []
+    depMap[str(token.dep_)].append({
+        "text": token.text,
+        "test pos": str(token.pos_),
+        "head": token.head.text,
+        "head pos": str(token.head.pos_)
+    })
+
+
+# Some dependency errors:
+# print(depMap.keys())
+# print([w['text'] for w in depMap['poss'] if w['text'] not in ['my', 'My', 'your', 'Your', 'his', 'His',
+#                                                              'her', 'Her', 'their', 'Their', 'our', 'Our', 'its']])
+
+# print([w['text'] for w in depMap['cc'] if w['text'].lower() not in ['and', 'but', 'for', 'or', 'so', 'nor', 'yet',
+#                                                                    '&']])
---
+++ Assignment2/README.md
@@ -0,0 +1,5 @@
+The code for problem 1 can be run with:
+
+`python Problem1/problem1.py`
+
+
